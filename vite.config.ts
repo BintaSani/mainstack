9f8e12cf41e62713b9ea5ef9dc4da94c65515e3c
BintaSani <bintasani992@gmail.com>
2025-10-19 03:29:13 +0100

    build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,19 @@ export default defineConfig({
     setupFiles: "./setupTests.ts",
     css: true,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
