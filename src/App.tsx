c3dae97e24ed5ed4ee5b5daad314c449b47af924
BintaSani <bintasani992@gmail.com>
2025-10-19 03:41:18 +0100

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DashboardLayout from "./shared/layouts/dashboardLayout";
 import { FilterProvider } from "./shared/context/filterContext";
+import Home from "./pages/home";
 import Revenue from "./pages/revenuePage";
 import "./App.css";
 
@@ -14,7 +15,10 @@ function App() {
         <FilterProvider>
           <Routes>
             <Route element={<DashboardLayout />}>
+              <Route path="/" element={<Home />} />
               <Route path="/revenue" element={<Revenue />} />
+              <Route path="/analytics" element={<Home />} />
+              <Route path="/crm" element={<Home />} />
             </Route>
           </Routes>
         </FilterProvider>
---
+++ src/pages/home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Path</div>;
+}
+
+export default Home;
